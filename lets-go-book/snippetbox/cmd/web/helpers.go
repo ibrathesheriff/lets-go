b408b9fa97eea7df986129c9d8dd9995b035aa4a
ibrathesheriff <ibrasururu@gmail.com>
2025-06-10 18:10:32 +0200

    Let's Go: Chapter 11.7 - CSRF Protection added! With form submission working.

--- lets-go-book/snippetbox/cmd/web/helpers.go
+++ lets-go-book/snippetbox/cmd/web/helpers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 )
 
 // Create a new decodePostForm() helper method. The second parameter here, dst,
@@ -108,6 +109,7 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 		Flash: app.sessionManager.PopString(r.Context(), "flash"),
 		// Add the authentication status to the template data.
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken: nosurf.Token(r),  // Add the CSRF token.
 	}
 }
 
--- lets-go-book/snippetbox/cmd/web/templates.go
+++ lets-go-book/snippetbox/cmd/web/templates.go
@@ -15,6 +15,7 @@ type templateData struct {
 	Form any
 	Flash string  // Add a Flash field to the templateData struct.
 	IsAuthenticated bool  // Add an IsAuthenticated field to the templateData struct
+	CSRFToken string  // Add CSRFToken field.
 }
 
 // Create a humanDate function which returns a nicely formatted string
